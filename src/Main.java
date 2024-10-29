f361d3144d7947ac13dc974f9ca2be11ede9ed97
michaelbohmann <bohmanmh@mail.uc.edu>
2024-10-29 14:23:05 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args) {
+        String[] users = new String[10]; // declare users to hold 10 strings
+        String[] DOW = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+
+        for(String u : DOW)
+            System.out.println(u);
+    }
+}
